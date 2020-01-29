11d8d905ceee5f2533b2e39addfd077a99359211
om <omprakashmishra3978@gmail.com>
2020-01-29 15:15:03 -0800

    Added health endpoint for liveness probe

--- api/src/main/java/ca/bc/gov/educ/api/digitalid/controller/DigitalIDController.java
+++ api/src/main/java/ca/bc/gov/educ/api/digitalid/controller/DigitalIDController.java
@@ -4,6 +4,9 @@ import ca.bc.gov.educ.api.digitalid.endpoint.DigitalIDEndpoint;
 import ca.bc.gov.educ.api.digitalid.mappers.DigitalIDEntityMapper;
 import ca.bc.gov.educ.api.digitalid.service.DigitalIDService;
 import ca.bc.gov.educ.api.digitalid.struct.DigitalID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -24,29 +27,37 @@ import java.util.UUID;
  */
 
 @RestController
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableResourceServer
+@Slf4j
 public class DigitalIDController implements DigitalIDEndpoint {
-    private final DigitalIDService service;
-    final DigitalIDEntityMapper mapper = DigitalIDEntityMapper.mapper;
 
-    DigitalIDController(@Autowired final DigitalIDService digitalIDService) {
-        this.service = digitalIDService;
-    }
+  @Getter(AccessLevel.PRIVATE)
+  private final DigitalIDService service;
+  final DigitalIDEntityMapper mapper = DigitalIDEntityMapper.mapper;
 
-    public DigitalID searchDigitalID(@RequestParam("identitytype") String typeCode, @RequestParam("identityvalue") String typeValue) {
-        return mapper.toStructure(service.searchDigitalId(typeCode, typeValue));
-    }
+  DigitalIDController(@Autowired final DigitalIDService digitalIDService) {
+    this.service = digitalIDService;
+  }
 
-    public DigitalID retrieveDigitalID(@PathVariable String id) {
-        return mapper.toStructure(service.retrieveDigitalID(UUID.fromString(id)));
-    }
+  public DigitalID searchDigitalID(@RequestParam("identitytype") String typeCode, @RequestParam("identityvalue") String typeValue) {
+    return mapper.toStructure(service.searchDigitalId(typeCode, typeValue));
+  }
 
-    public DigitalID createDigitalID(@Validated @RequestBody DigitalID digitalID) {
-        return mapper.toStructure(service.createDigitalID(mapper.toModel(digitalID)));
-    }
+  public DigitalID retrieveDigitalID(@PathVariable String id) {
+    return mapper.toStructure(service.retrieveDigitalID(UUID.fromString(id)));
+  }
 
-    public DigitalID updateDigitalID(@Validated @RequestBody DigitalID digitalID) {
-        return mapper.toStructure(service.updateDigitalID(mapper.toModel(digitalID)));
-    }
+  public DigitalID createDigitalID(@Validated @RequestBody DigitalID digitalID) {
+    return mapper.toStructure(service.createDigitalID(mapper.toModel(digitalID)));
+  }
+
+  public DigitalID updateDigitalID(@Validated @RequestBody DigitalID digitalID) {
+    return mapper.toStructure(service.updateDigitalID(mapper.toModel(digitalID)));
+  }
+
+  @Override
+  public String health() {
+    log.info("Health Check OK, returning OK");
+    return "OK";
+  }
 }
