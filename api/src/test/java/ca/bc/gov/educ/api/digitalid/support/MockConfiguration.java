cd376872a0c91aef93bb3e9030cd05cd633d16d8
mishraomp <omprakashmishra3978@gmail.com>
2020-05-09 11:11:59 -0700

    fixes to run unit tests

---
+++ api/src/test/java/ca/bc/gov/educ/api/digitalid/support/MockConfiguration.java
@@ -0,0 +1,34 @@
+package ca.bc.gov.educ.api.digitalid.support;
+
+
+import ca.bc.gov.educ.api.digitalid.messaging.MessagePublisher;
+import ca.bc.gov.educ.api.digitalid.messaging.MessageSubscriber;
+import ca.bc.gov.educ.api.digitalid.poll.EventTaskScheduler;
+import org.mockito.Mockito;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+@Profile("test")
+@Configuration
+public class MockConfiguration {
+    @Bean
+    @Primary
+    public MessagePublisher messagePublisher() {
+        return Mockito.mock(MessagePublisher.class);
+    }
+
+    @Bean
+    @Primary
+    public MessageSubscriber messageSubscriber() {
+        return Mockito.mock(MessageSubscriber.class);
+    }
+
+    @Bean
+    @Primary
+    public EventTaskScheduler eventTaskScheduler() {
+        return Mockito.mock(EventTaskScheduler.class);
+    }
+
+}
