db3e0773d67277286d7a8328236f9642c74a3fce
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2020-07-04 16:52:45 -0700

    added saga name to complete message

--- api/src/main/java/ca/bc/gov/educ/api/digitalid/struct/Event.java
+++ api/src/main/java/ca/bc/gov/educ/api/digitalid/struct/Event.java
@@ -2,6 +2,7 @@ package ca.bc.gov.educ.api.digitalid.struct;
 
 import ca.bc.gov.educ.api.digitalid.constants.EventOutcome;
 import ca.bc.gov.educ.api.digitalid.constants.EventType;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @Builder
 @NoArgsConstructor
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Event {
   private EventType eventType;
   private EventOutcome eventOutcome;
