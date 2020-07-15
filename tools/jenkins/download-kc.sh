1b5b1862ca363220838c7e69e91540683aeed505
Marco Villeneuve <marco.villeneuve@gov.bc.ca>
2020-07-15 07:54:08 -0700

    Added new changes for pipeline.

--- tools/jenkins/download-kc.sh
+++
@@ -1,9 +0,0 @@
-namespace=$1
-
-if [ ! -f /tmp/keycloak-9.0.3.zip ]
-then
-    echo 'Downloading and unzipping keycloak setup files...'
-    wget -P /tmp -nc https://downloads.jboss.org/keycloak/9.0.3/keycloak-9.0.3.zip
-    cd /tmp
-    unzip keycloak-9.0.3.zip
-fi
