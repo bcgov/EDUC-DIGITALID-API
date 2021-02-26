74579b7b3e8cf734d6a8a21c70b843ee24207693
MTASFIN <mahditasfin1998@gmail.com>
2021-02-25 17:24:22 -0800

    Refer to PEN-1219

--- api/src/test/java/ca/bc/gov/educ/api/digitalid/controller/DigitalIDControllerTest.java
+++ api/src/test/java/ca/bc/gov/educ/api/digitalid/controller/DigitalIDControllerTest.java
@@ -9,13 +9,13 @@ import ca.bc.gov.educ.api.digitalid.repository.DigitalIDRepository;
 import ca.bc.gov.educ.api.digitalid.repository.IdentityTypeCodeTableRepository;
 import ca.bc.gov.educ.api.digitalid.service.DigitalIDService;
 import ca.bc.gov.educ.api.digitalid.struct.DigitalID;
-import ca.bc.gov.educ.api.digitalid.support.WithMockOAuth2Scope;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
@@ -28,6 +28,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -35,6 +36,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class DigitalIDControllerTest {
   @Autowired
@@ -46,6 +48,7 @@ public class DigitalIDControllerTest {
   @Autowired
   DigitalIDRepository repository;
 
+  @Autowired
   private MockMvc mockMvc;
 
   @Autowired
@@ -56,8 +59,6 @@ public class DigitalIDControllerTest {
 
   @Before
   public void setUp() {
-    mockMvc = MockMvcBuilders.standaloneSetup(controller)
-            .setControllerAdvice(new RestExceptionHandler()).build();
     identityTypeCodeTableRepository.save(createIdentityTypeCodeData());
     accessChannelCodeTableRepository.save(createAccessChannelCodeData());
   }
@@ -86,141 +87,123 @@ public class DigitalIDControllerTest {
 
 
   @Test
-  @WithMockOAuth2Scope(scope = "READ_DIGITALID")
   public void testRetrieveDigitalId_GivenRandomID_ShouldThrowEntityNotFoundException() throws Exception {
-    this.mockMvc.perform(get("/" + UUID.randomUUID())).andDo(print()).andExpect(status().isNotFound());
+    this.mockMvc.perform(get("/" + UUID.randomUUID()).with(jwt().jwt((jwt) -> jwt.claim("scope", "READ_DIGITALID")))).andDo(print()).andExpect(status().isNotFound());
   }
 
 
   @Test
-  @WithMockOAuth2Scope(scope = "READ_DIGITALID")
   public void testRetrieveDigitalId_GivenValidID_ShouldReturnOK() throws Exception {
     DigitalIDEntity entity = service.createDigitalID(createDigitalIDMockData());
-    this.mockMvc.perform(get("/" + entity.getDigitalID())).andDo(print()).andExpect(status().isOk());
+    this.mockMvc.perform(get("/" + entity.getDigitalID()).with(jwt().jwt((jwt) -> jwt.claim("scope", "READ_DIGITALID")))).andDo(print()).andExpect(status().isOk());
     repository.deleteById(entity.getDigitalID());
   }
 
   @Test
-  @WithMockOAuth2Scope(scope = "READ_DIGITALID_CODETABLE")
   public void testRetrieveIdentityTypeCodes_GivenARecordInDB_ShouldReturnOK() throws Exception {
-    this.mockMvc.perform(get("/identityTypeCodes")).andDo(print()).andExpect(status().isOk()).andExpect(jsonPath("$", hasSize(1)));
+    this.mockMvc.perform(get("/identityTypeCodes").with(jwt().jwt((jwt) -> jwt.claim("scope", "READ_DIGITALID_CODETABLE")))).andDo(print()).andExpect(status().isOk()).andExpect(jsonPath("$", hasSize(1)));
   }
 
   @Test
-  @WithMockOAuth2Scope(scope = "READ_DIGITALID_CODETABLE")
   public void testRetrieveAccessChannelCodes_GivenARecordInDB_ShouldReturnOK() throws Exception {
-    this.mockMvc.perform(get("/accessChannelCodes")).andDo(print()).andExpect(status().isOk()).andExpect(jsonPath("$", hasSize(1)));
+    this.mockMvc.perform(get("/accessChannelCodes").with(jwt().jwt((jwt) -> jwt.claim("scope", "READ_DIGITALID_CODETABLE")))).andDo(print()).andExpect(status().isOk()).andExpect(jsonPath("$", hasSize(1)));
   }
 
   @Test
-  @WithMockOAuth2Scope(scope = "WRITE_DIGITALID")
   public void testCreateDigitalId_GivenValidPayload_ShouldReturnStatusCreated() throws Exception {
-    this.mockMvc.perform(post("/").contentType(MediaType.APPLICATION_JSON)
+    this.mockMvc.perform(post("/").with(jwt().jwt((jwt) -> jwt.claim("scope", "WRITE_DIGITALID"))).contentType(MediaType.APPLICATION_JSON)
             .accept(MediaType.APPLICATION_JSON).content(asJsonString(DigitalID.builder().identityTypeCode("BCSC").lastAccessChannelCode("AC")
                     .createUser("TEST").updateUser("TEST").identityValue("Test1").lastAccessDate(LocalDateTime.now().toString()).build()))).andDo(print()).andExpect(status().isCreated());
   }
 
   @Test
-  @WithMockOAuth2Scope(scope = "WRITE_DIGITALID")
   public void testCreateDigitalId_GivenDigitalIdInPayload_ShouldReturnStatusBadRequest() throws Exception {
-    this.mockMvc.perform(post("/").contentType(MediaType.APPLICATION_JSON)
+    this.mockMvc.perform(post("/").with(jwt().jwt((jwt) -> jwt.claim("scope", "WRITE_DIGITALID"))).contentType(MediaType.APPLICATION_JSON)
             .accept(MediaType.APPLICATION_JSON).content(asJsonString(DigitalID.builder().digitalID(UUID.randomUUID().toString()).identityTypeCode("BCSC").lastAccessChannelCode("AC")
                     .createUser("TEST").updateUser("TEST").identityValue("Test1").lastAccessDate(LocalDateTime.now().toString()).build()))).andDo(print()).andExpect(status().isBadRequest());
   }
 
   @Test
-  @WithMockOAuth2Scope(scope = "WRITE_DIGITALID")
   public void testCreateDigitalId_GivenInvalidLACInPayload_ShouldReturnStatusBadRequest() throws Exception {
-    this.mockMvc.perform(post("/").contentType(MediaType.APPLICATION_JSON)
+    this.mockMvc.perform(post("/").with(jwt().jwt((jwt) -> jwt.claim("scope", "WRITE_DIGITALID"))).contentType(MediaType.APPLICATION_JSON)
             .accept(MediaType.APPLICATION_JSON).content(asJsonString(DigitalID.builder().identityTypeCode("BCSC").lastAccessChannelCode("AC1")
                     .createUser("TEST").updateUser("TEST").identityValue("Test1").lastAccessDate(LocalDateTime.now().toString()).build()))).andDo(print()).andExpect(status().isBadRequest());
   }
 
   @Test
-  @WithMockOAuth2Scope(scope = "WRITE_DIGITALID")
   public void testCreateDigitalId_GivenInvalidLITCInPayload_ShouldReturnStatusBadRequest() throws Exception {
-    this.mockMvc.perform(post("/").contentType(MediaType.APPLICATION_JSON)
+    this.mockMvc.perform(post("/").with(jwt().jwt((jwt) -> jwt.claim("scope", "WRITE_DIGITALID"))).contentType(MediaType.APPLICATION_JSON)
             .accept(MediaType.APPLICATION_JSON).content(asJsonString(DigitalID.builder().identityTypeCode("BCSC1").lastAccessChannelCode("AC")
                     .createUser("TEST").updateUser("TEST").identityValue("Test1").lastAccessDate(LocalDateTime.now().toString()).build()))).andDo(print()).andExpect(status().isBadRequest());
   }
 
   @Test
-  @WithMockOAuth2Scope(scope = "WRITE_DIGITALID")
   public void testCreateDigitalId_GivenITCIsNull_ShouldReturnStatusBadRequest() throws Exception {
-    this.mockMvc.perform(post("/").contentType(MediaType.APPLICATION_JSON)
+    this.mockMvc.perform(post("/").with(jwt().jwt((jwt) -> jwt.claim("scope", "WRITE_DIGITALID"))).contentType(MediaType.APPLICATION_JSON)
             .accept(MediaType.APPLICATION_JSON).content(asJsonString(DigitalID.builder().identityTypeCode(null).lastAccessChannelCode("AC")
                     .createUser("TEST").updateUser("TEST").identityValue("Test1").lastAccessDate(LocalDateTime.now().toString()).build()))).andDo(print()).andExpect(status().isBadRequest());
   }
 
   @Test
-  @WithMockOAuth2Scope(scope = "WRITE_DIGITALID")
   public void testCreateDigitalId_GivenITVIsNull_ShouldReturnStatusBadRequest() throws Exception {
-    this.mockMvc.perform(post("/").contentType(MediaType.APPLICATION_JSON)
+    this.mockMvc.perform(post("/").with(jwt().jwt((jwt) -> jwt.claim("scope", "WRITE_DIGITALIDE"))).contentType(MediaType.APPLICATION_JSON)
             .accept(MediaType.APPLICATION_JSON).content(asJsonString(DigitalID.builder().identityTypeCode("BCSC").lastAccessChannelCode("AC")
                     .createUser("TEST").updateUser("TEST").identityValue(null).lastAccessDate(LocalDateTime.now().toString()).build()))).andDo(print()).andExpect(status().isBadRequest());
   }
   @Test
-  @WithMockOAuth2Scope(scope = "WRITE_DIGITALID")
   public void testCreateDigitalId_GivenLADFormatWrong_ShouldReturnStatusBadRequest() throws Exception {
-    this.mockMvc.perform(post("/").contentType(MediaType.APPLICATION_JSON)
+    this.mockMvc.perform(post("/").with(jwt().jwt((jwt) -> jwt.claim("scope", "WRITE_DIGITALID"))).contentType(MediaType.APPLICATION_JSON)
             .accept(MediaType.APPLICATION_JSON).content(asJsonString(DigitalID.builder().identityTypeCode("BCSC").lastAccessChannelCode("AC")
                     .createUser("TEST").updateUser("TEST").identityValue("Test1").lastAccessDate("2020-01-0119:40:09").build()))).andDo(print()).andExpect(status().isBadRequest());
   }
   @Test
-  @WithMockOAuth2Scope(scope = "WRITE_DIGITALID")
   public void testCreateDigitalId_GivenLADInFuture_ShouldReturnStatusBadRequest() throws Exception {
-    this.mockMvc.perform(post("/").contentType(MediaType.APPLICATION_JSON)
+    this.mockMvc.perform(post("/").with(jwt().jwt((jwt) -> jwt.claim("scope", "WRITE_DIGITALID"))).contentType(MediaType.APPLICATION_JSON)
             .accept(MediaType.APPLICATION_JSON).content(asJsonString(DigitalID.builder().identityTypeCode("BCSC").lastAccessChannelCode("AC")
                     .createUser("TEST").updateUser("TEST").identityValue("Test1").lastAccessDate("2199-01-01T19:40:09").build()))).andDo(print()).andExpect(status().isBadRequest());
   }
   @Test
-  @WithMockOAuth2Scope(scope = "WRITE_DIGITALID")
   public void testCreateDigitalId_GivenLACIsNull_ShouldReturnStatusBadRequest() throws Exception {
-    this.mockMvc.perform(post("/").contentType(MediaType.APPLICATION_JSON)
+    this.mockMvc.perform(post("/").with(jwt().jwt((jwt) -> jwt.claim("scope", "WRITE_DIGITALID"))).contentType(MediaType.APPLICATION_JSON)
             .accept(MediaType.APPLICATION_JSON).content(asJsonString(DigitalID.builder().identityTypeCode("BCSC").lastAccessChannelCode(null)
                     .createUser("TEST").updateUser("TEST").identityValue("Test1").lastAccessDate(LocalDateTime.now().toString()).build()))).andDo(print()).andExpect(status().isBadRequest());
   }
 
   @Test
-  @WithMockOAuth2Scope(scope = "WRITE_DIGITALID")
   public void testCreateDigitalId_GivenLADIsNull_ShouldReturnStatusBadRequest() throws Exception {
-    this.mockMvc.perform(post("/").contentType(MediaType.APPLICATION_JSON)
+    this.mockMvc.perform(post("/").with(jwt().jwt((jwt) -> jwt.claim("scope", "WRITE_DIGITALID"))).contentType(MediaType.APPLICATION_JSON)
             .accept(MediaType.APPLICATION_JSON).content(asJsonString(DigitalID.builder().identityTypeCode("BCSC").lastAccessChannelCode("AC")
                     .createUser("TEST").updateUser("TEST").identityValue("Test1").lastAccessDate(null).build()))).andDo(print()).andExpect(status().isBadRequest());
   }
 
   @Test
-  @WithMockOAuth2Scope(scope = "WRITE_DIGITALID")
   public void testUpdateDigitalId_GivenRandomDigitalIDInPayload_ShouldReturnStatusNotFound() throws Exception {
-    this.mockMvc.perform(put("/").contentType(MediaType.APPLICATION_JSON)
+    this.mockMvc.perform(put("/").with(jwt().jwt((jwt) -> jwt.claim("scope", "WRITE_DIGITALID"))).contentType(MediaType.APPLICATION_JSON)
             .accept(MediaType.APPLICATION_JSON).content(asJsonString(DigitalID.builder().digitalID(UUID.randomUUID().toString()).identityTypeCode("BCSC").lastAccessChannelCode("AC")
                     .createUser("TEST").updateUser("TEST").identityValue("Test1").lastAccessDate(LocalDateTime.now().toString()).build()))).andDo(print()).andExpect(status().isNotFound());
   }
 
   @Test
-  @WithMockOAuth2Scope(scope = "WRITE_DIGITALID")
   public void testUpdateDigitalId_GivenValidPayload_ShouldReturnStatusOK() throws Exception {
     DigitalIDEntity entity = service.createDigitalID(createDigitalIDMockData());
-    this.mockMvc.perform(put("/").contentType(MediaType.APPLICATION_JSON)
+    this.mockMvc.perform(put("/").with(jwt().jwt((jwt) -> jwt.claim("scope", "WRITE_DIGITALID"))).contentType(MediaType.APPLICATION_JSON)
             .accept(MediaType.APPLICATION_JSON).content(asJsonString(DigitalID.builder().digitalID(entity.getDigitalID().toString()).identityTypeCode("BCSC").lastAccessChannelCode("AC")
                     .createUser("TEST").updateUser("TEST").identityValue("Test").lastAccessDate(LocalDateTime.now().toString()).build()))).andDo(print()).andExpect(status().isOk()).andExpect(MockMvcResultMatchers.jsonPath("$.identityValue").value("TEST"));
   }
 
   @Test
-  @WithMockOAuth2Scope(scope = "WRITE_DIGITALID")
   public void testUpdateDigitalId_GivenInvalidLITCInPayload_ShouldReturnStatusBadRequest() throws Exception {
     DigitalIDEntity entity = service.createDigitalID(createDigitalIDMockData());
-    this.mockMvc.perform(put("/").contentType(MediaType.APPLICATION_JSON)
+    this.mockMvc.perform(put("/").with(jwt().jwt((jwt) -> jwt.claim("scope", "WRITE_DIGITALID"))).contentType(MediaType.APPLICATION_JSON)
             .accept(MediaType.APPLICATION_JSON).content(asJsonString(DigitalID.builder().digitalID(entity.getDigitalID().toString()).identityTypeCode("BCSC1").lastAccessChannelCode("AC")
                     .createUser("TEST").updateUser("TEST").identityValue("Test1").lastAccessDate(LocalDateTime.now().toString()).build()))).andDo(print()).andExpect(status().isBadRequest());
   }
 
 
   @Test
-  @WithMockOAuth2Scope(scope = "READ_DIGITALID")
   public void testSearchDigitalId_GivenIdentityTypeAndIdentityValueExistInDB_ShouldReturnStatusOK() throws Exception {
     DigitalIDEntity entity = service.createDigitalID(createDigitalIDMockData());
-    this.mockMvc.perform(get("/").param("identitytype", entity.getIdentityTypeCode()).param("identityvalue", entity.getIdentityValue()).contentType(MediaType.APPLICATION_JSON)
+    this.mockMvc.perform(get("/").with(jwt().jwt((jwt) -> jwt.claim("scope", "READ_DIGITALID"))).param("identitytype", entity.getIdentityTypeCode()).param("identityvalue", entity.getIdentityValue()).contentType(MediaType.APPLICATION_JSON)
             .accept(MediaType.APPLICATION_JSON)).andDo(print()).andExpect(status().isOk()).andExpect(MockMvcResultMatchers.jsonPath("$.identityValue").value("123"));
   }
 
