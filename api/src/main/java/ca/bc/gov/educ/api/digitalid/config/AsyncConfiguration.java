580f339a787f94b43c1f07e5e73d579ceffeb992
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2021-02-25 18:24:36 -0800

    compilation fixes...

--- api/src/main/java/ca/bc/gov/educ/api/digitalid/config/AsyncConfiguration.java
+++ api/src/main/java/ca/bc/gov/educ/api/digitalid/config/AsyncConfiguration.java
@@ -1,14 +1,14 @@
 package ca.bc.gov.educ.api.digitalid.config;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import org.jboss.threads.EnhancedQueueExecutor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.jboss.threads.EnhancedQueueExecutor;
+
+import java.time.Duration;
 import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
 
 @Configuration
 @EnableAsync
