c43cc91a70c5794a67eb1fe36deeca8457bbd828
OMPRAKASH MISHRA <32200996+mishraomp@users.noreply.github.com>
2021-02-25 18:18:24 -0800

    Update AsyncConfiguration.java

--- api/src/main/java/ca/bc/gov/educ/api/digitalid/config/AsyncConfiguration.java
+++ api/src/main/java/ca/bc/gov/educ/api/digitalid/config/AsyncConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableAsync;
-
+import org.jboss.threads.EnhancedQueueExecutor;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
