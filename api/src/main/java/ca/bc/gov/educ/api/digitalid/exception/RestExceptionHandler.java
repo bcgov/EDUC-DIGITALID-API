11598e5fce8b0eaad93986b0b4bc6bccb153850c
OMPRAKASH MISHRA <32200996+mishraomp@users.noreply.github.com>
2021-05-27 16:54:48 -0700

    Update RestExceptionHandler.java

--- api/src/main/java/ca/bc/gov/educ/api/digitalid/exception/RestExceptionHandler.java
+++ api/src/main/java/ca/bc/gov/educ/api/digitalid/exception/RestExceptionHandler.java
@@ -100,7 +100,7 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
           EntityNotFoundException ex) {
     ApiError apiError = new ApiError(NOT_FOUND);
     apiError.setMessage(ex.getMessage());
-    log.debug("{} ", apiError.getMessage(), ex);
+    log.info("{} ", apiError.getMessage(), ex);
     return buildResponseEntity(apiError);
   }
 
