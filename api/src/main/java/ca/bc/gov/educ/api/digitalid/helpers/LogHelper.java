35c124eedc75b076a13b61a9a9531635e1481313
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2021-06-10 21:50:34 -0700

    log and correlation-id changes

--- api/src/main/java/ca/bc/gov/educ/api/digitalid/helpers/LogHelper.java
+++ api/src/main/java/ca/bc/gov/educ/api/digitalid/helpers/LogHelper.java
@@ -5,8 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.slf4j.MDC;
 import org.springframework.lang.NonNull;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -24,21 +22,17 @@ public final class LogHelper {
 
   public static void logServerHttpReqResponseDetails(@NonNull final HttpServletRequest request, final HttpServletResponse response) {
     try {
-      JwtAuthenticationToken jwt = (JwtAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
       final int status = response.getStatus();
       val totalTime = Instant.now().toEpochMilli() - (Long) request.getAttribute("startTime");
       final Map<String, Object> httpMap = new HashMap<>();
       httpMap.put("server_http_response_code", status);
       httpMap.put("server_http_request_method", request.getMethod());
       httpMap.put("server_http_query_params", request.getQueryString());
-      httpMap.put("correlation_id", request.getHeader("correlationID"));
+      httpMap.put("server_http_request_correlation_id", request.getHeader("correlationID"));
       httpMap.put("server_http_request_url", String.valueOf(request.getRequestURL()));
       httpMap.put("server_http_request_processing_time_ms", totalTime);
       httpMap.put("server_http_request_payload", String.valueOf(request.getAttribute("payload")));
       httpMap.put("server_http_request_remote_address", request.getRemoteAddr());
-      if(jwt != null){
-        httpMap.put("server_http_request_client_id", jwt.getToken().getClaim("clientId"));
-      }
       MDC.putCloseable("httpEvent", mapper.writeValueAsString(httpMap));
       log.info("");
       MDC.clear();
