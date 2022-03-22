11e3c77d1fed2cd3e0408d495d4267add6b11add
Mingwei <mingwei.y@gmail.com>
2022-03-21 17:42:19 -0700

    PEN-1661: Add new scheduler to delete event records

---
+++ api/src/test/resources/schema.sql
@@ -0,0 +1,2 @@
+CREATE TABLE DIGITAL_ID_SHEDLOCK(name VARCHAR(64) NOT NULL, lock_until TIMESTAMP(3) NOT NULL,
+                                        locked_at TIMESTAMP(3) NOT NULL, locked_by VARCHAR(255) NOT NULL, PRIMARY KEY (name));
