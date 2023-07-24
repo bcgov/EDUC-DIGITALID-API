c7b37dffba0fd7cbfa776a4f497d8ccc1bea47cc
arcshiftsolutions <arcshiftsolutions@gmail.com>
2023-07-24 15:47:42 -0700

    Added Entra as login type

---
+++ api/src/main/resources/db/migration/V1.0.6__DIGITALID_API.sql
@@ -0,0 +1,5 @@
+INSERT INTO IDENTITY_TYPE_CODE (IDENTITY_TYPE_CODE, LABEL, DESCRIPTION, DISPLAY_ORDER, EFFECTIVE_DATE, EXPIRY_DATE,
+                                CREATE_USER, CREATE_DATE, UPDATE_USER, UPDATE_DATE)
+VALUES ('ENTRA', 'Microsoft Entra', 'Digital Identity via Entra', 4,
+        to_date('2020-01-01', 'YYYY-MM-DD'), to_date('2099-12-31', 'YYYY-MM-DD'), 'IDIR/MVILLENE',
+        to_date('2019-11-07', 'YYYY-MM-DD'), 'IDIR/MVILLENE', to_date('2019-11-07', 'YYYY-MM-DD'));
